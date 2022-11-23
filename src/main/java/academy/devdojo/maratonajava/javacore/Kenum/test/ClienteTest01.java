de9b873596cbba07f5e36eacce4ad55b89fc5430
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-11-23 12:41:19 -0300

    82 - Orientação Objetos - Enumeração pt 03 - Sobrescrita de métodos

--- src/main/java/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,8 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
-import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
@@ -10,5 +10,7 @@ public class ClienteTest01 {
         Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.DEBITO);
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
