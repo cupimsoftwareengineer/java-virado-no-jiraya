fce2527483866372b8f112a628fc69fc8598be87
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-11-23 21:00:48 -0300

    87 - Orientação Objetos - Interfaces pt 01 - Introdução

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.test;
+
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
+
+public class DataLoaderTest01 {
+    public void main (String[] args) {
+        DatabaseLoader databaseLoader = new DatabaseLoader();
+        FileLoader fileLoader = new FileLoader();
+    }
+}
