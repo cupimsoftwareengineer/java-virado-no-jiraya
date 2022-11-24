c017f37116a4e481d41d782a40f143255fabd907
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-11-23 21:03:04 -0300

    87 - Orientação Objetos - Interfaces pt 01 - imprimindo na tela

--- src/main/java/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -4,8 +4,10 @@ import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
 import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
 
 public class DataLoaderTest01 {
-    public void main (String[] args) {
+    public static void main (String[] args) {
         DatabaseLoader databaseLoader = new DatabaseLoader();
         FileLoader fileLoader = new FileLoader();
+        databaseLoader.load();
+        fileLoader.load();
     }
 }
