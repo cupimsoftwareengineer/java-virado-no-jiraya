9a4f2f9f15510aad1d92c83c888a03e19cbc7802
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-12-07 13:53:37 -0300

    132 - Classes Utilitárias - Regex pt 02 - Pattern e Matcher - Meta caracteres

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = todos os dígitos
+        // \D = Tudo o que não for dígito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-ZA-Z, dígitos, _
+        // \W = Tudo o que não for incluso \w
+        // []
+
+        // String regex = "[a-zA-C]";
+         String regex = "0[xX][0-9a-fA-F]";
+        // String texto = "abaaba";
+        String texto2 = "12 0x 0X 0xFFABC 0x109 0x1";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto2);
+
+        System.out.println("Texto: " + texto2);
+        System.out.println("Indice: 0123456789");
+        System.out.println("Regex: " + texto2);
+        System.out.println("Posicoes encontradas");
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+    }
+}
