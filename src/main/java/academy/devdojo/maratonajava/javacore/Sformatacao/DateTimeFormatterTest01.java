457c61a3646d27bb288a3ee1893548261be2210e
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-12-07 00:16:47 -0300

    129 - Classes Utilitárias - DateTimeFormatter

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Sformatacao/DateTimeFormatterTest01.java
@@ -0,0 +1,47 @@
+package academy.devdojo.maratonajava.javacore.Sformatacao;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Local;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeFormatterTest01 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        // Parse
+        System.out.println("\nParse");
+        LocalDate parse1 = LocalDate.parse("20221206", DateTimeFormatter.BASIC_ISO_DATE);
+        LocalDate parse2 = LocalDate.parse("2022-12-06+23:48", DateTimeFormatter.ISO_DATE);
+        LocalDate parse3 = LocalDate.parse("2022-12-06", DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(parse1);
+        System.out.println(parse2);
+        System.out.println(parse3);
+
+        LocalDateTime now = LocalDateTime.now();
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+        LocalDateTime parse4 = LocalDateTime.parse("2022-12-07T00:05:27.79497", DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(parse4);
+
+        DateTimeFormatter formatterBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formatBR = LocalDate.now().format(formatterBR);
+        System.out.println(formatBR);
+        LocalDate parseBR = LocalDate.parse("19/02/2021", formatterBR);
+        System.out.println(parseBR);
+
+        DateTimeFormatter formatterGR = DateTimeFormatter.ofPattern("dd.MMMM.yyyy", Locale.GERMAN);
+        String formatGR = LocalDate.now().format(formatterGR);
+        System.out.println(formatGR);
+
+    }
+}
