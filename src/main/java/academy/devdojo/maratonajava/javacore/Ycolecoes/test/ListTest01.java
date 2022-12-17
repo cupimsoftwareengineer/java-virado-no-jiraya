f694099d4ae7c92495a13eb80805afda8178df31
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-12-17 19:40:58 -0300

    166 - Coleções pt 06 - List pt 01

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList();
+        names.add("Cupim");
+        names.add("Luan");
+
+        for (String name: names) {
+            System.out.println(name);
+        }
+
+        System.out.println("---------");
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+    }
+}
