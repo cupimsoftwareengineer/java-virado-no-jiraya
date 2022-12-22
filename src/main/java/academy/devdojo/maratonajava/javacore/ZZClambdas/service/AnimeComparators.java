f7c928ba9b7f5524054628e0ad1d0fa3a4b72e61
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-12-22 15:07:06 -0300

    199 - Method Reference pt 02 - Referência a métodos não estáticos

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZClambdas/service/AnimeComparators.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZClambdas/service/AnimeComparators.java
@@ -6,4 +6,8 @@ public class AnimeComparators {
   public static int compareByTitle(Anime a1, Anime a2) {
     return a1.getTitle().compareTo(a2.getTitle());
   }
+
+  public int compareByTitleNonStatic(Anime a1, Anime a2) {
+    return a1.getTitle().compareTo(a2.getTitle());
+  }
 }
--- src/main/java/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -6,12 +6,13 @@ import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators
 import java.util.ArrayList;
 import java.util.List;
 
-// Reference to a static method
-public class MethodReferenceTest01 {
+// Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
   public static void main(String[] args) {
+    AnimeComparators animeComparators = new AnimeComparators();
     List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One piece", 900), new Anime("Naruto", 500)));
-//    Collections.sort(animeList, (a1, a2) -> a1.getTitle().compareTo(a2.getTitle()));
-    animeList.sort(AnimeComparators::compareByTitle);
+    animeList.sort(animeComparators::compareByTitleNonStatic);
+    animeList.sort((a1, a2) -> animeComparators.compareByTitleNonStatic(a1, a2));
     System.out.println(animeList);
   }
 }
