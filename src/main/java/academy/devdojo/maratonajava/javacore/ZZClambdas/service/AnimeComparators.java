dbc2471b216b9da1e5216f979bdf93039f5c8e08
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-12-22 14:08:09 -0300

    198 - Method Reference pt 01 - Referência a métodos estáticos

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZClambdas/service/AnimeComparators.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.service;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+
+public class AnimeComparators {
+  public static int compareByTitle(Anime a1, Anime a2) {
+    return a1.getTitle().compareTo(a2.getTitle());
+  }
+}
