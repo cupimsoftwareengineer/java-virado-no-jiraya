13aaaf3c78768db0ebf55d65e211aab1f0e47f44
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-12-27 23:17:51 -0300

    258 - JDBC pt 07 - Lombok e Log4J2

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProduceRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProduceRepository.java
@@ -2,18 +2,20 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.repository;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Log4j2
 public class ProduceRepository {
   public static void save(Producer producer) {
     String sql = String.format("INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');", producer.getName());
     try (Connection conn = ConnectionFactory.getConnection();
          Statement stmt = conn.createStatement()) {
       int rowsAffected = stmt.executeUpdate(sql);
-      System.out.println(rowsAffected);
+      log.info("Inserted producer in the Database, rows affected '{}'", rowsAffected);
     } catch (SQLException e) {
       e.printStackTrace();
     }
