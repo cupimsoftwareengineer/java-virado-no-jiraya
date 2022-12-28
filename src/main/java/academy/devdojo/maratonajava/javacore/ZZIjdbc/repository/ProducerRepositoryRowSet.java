e2b735f5fc7734ee77aed8a73e2ca5aeb126e44c
cupimsoftwareengineer <cupimsoftwareengineer@outlook.com>
2022-12-28 16:25:55 -0300

    273 - JDBC pt 22 - Disconnected RowSet - CachedRowSet

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepositoryRowSet.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepositoryRowSet.java
@@ -4,10 +4,13 @@ import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.listener.CustomRowSetListener;
 
+import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.JdbcRowSet;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ProducerRepositoryRowSet {
 
@@ -31,18 +34,6 @@ public class ProducerRepositoryRowSet {
     }
     return producers;
   }
-//
-//    public static void updateJdbcRowSet(Producer producer) {
-//        String sql = "UPDATE `anime_store`.`producer` SET `name` = ? WHERE (`id` = ?);";
-//        try (JdbcRowSet jrs = ConnectionFactory.getJdbcRowSet()) {
-//            jrs.setCommand(sql);
-//            jrs.setString(1, producer.getName());
-//            jrs.setInt(2, producer.getId());
-//            jrs.execute();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
   public static void updateJdbcRowSet(Producer producer) {
     String sql = "SELECT * FROM anime_store.producer WHERE (`id` = ?);";
@@ -55,6 +46,24 @@ public class ProducerRepositoryRowSet {
       jrs.updateString("name", producer.getName());
       jrs.updateRow();
     } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void updateCachedRowSet(Producer producer) {
+    String sql = "SELECT * FROM producer WHERE (`id` = ?);";
+    try (CachedRowSet crs = ConnectionFactory.getCachedRowSet();
+         Connection connection = ConnectionFactory.getConnection()) {
+      connection.setAutoCommit(false);
+      crs.setCommand(sql);
+      crs.setInt(1, producer.getId());
+      crs.execute(connection);
+      if (!crs.next()) return;
+      crs.updateString("name", producer.getName());
+      crs.updateRow();
+      TimeUnit.SECONDS.sleep(10);
+      crs.acceptChanges();
+    } catch (SQLException | InterruptedException e) {
       e.printStackTrace();
     }
   }
